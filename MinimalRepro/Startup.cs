0ec229f804708270d2b1f356f12c62a6548933ab
jdubbeldam <jdubbeldam@independer.nl>
2022-09-22 15:14:27 +0200

    Add project files.

---
+++ MinimalRepro/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NServiceBus;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+[assembly: NServiceBusTriggerFunction(Startup.EndpointName)]
+internal class Startup : FunctionsStartup {
+  public const string EndpointName = "MyEndpoint";
+  public override void Configure(IFunctionsHostBuilder builder) {
+    builder.UseNServiceBus(c => {
+      var endpointConfiguration = new ServiceBusTriggeredEndpointConfiguration(EndpointName, c);
+      endpointConfiguration.Transport.UseWebSockets();
+      return endpointConfiguration;
+    });
+  }
+}
