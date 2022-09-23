5544cfb77dca469f410bdf88186a3ce2b5318af8
jdubbeldam <jdubbeldam@independer.nl>
2022-09-23 08:54:20 +0200

    added simple message handler to nservicebus version

---
+++ MinimalRepro/MessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System.Threading.Tasks;
+
+namespace MinimalRepro {
+  internal class MessageHandler : IHandleMessages<object> {
+    private readonly ILogger<MessageHandler> logger;
+
+    public MessageHandler(ILogger<MessageHandler> logger) {
+      this.logger = logger;
+    }
+    public Task Handle(object message, IMessageHandlerContext context) {
+      logger.LogInformation("Handled message of type {type}", message.GetType());
+      return Task.CompletedTask;
+    }
+  }
+}
